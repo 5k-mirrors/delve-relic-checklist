095245f152839d58487bfd177365b83e4d818a07
Csaba Apagyi <csaba.apagyi@gmail.com>
2018-09-14 17:03:11 +0200

    Adds logo

--- src/index.js
+++ src/index.js
@@ -4,14 +4,22 @@ import ReactDOM from "react-dom";
 import "./styles.css";
 import List from './list';
 
+import Background from '../public/background.png';
+
 class App extends React.Component {
   constructor(){
     super();
   }
 
   render() {
+    let backgroundStyle = {
+      backgroundImage: `url(${Background})`,
+      backgroundRepeat: "no-repeat",
+      backgroundSize: "100px"
+    };
+
     return (
-      <div className="App">
+      <div className="App" style={ backgroundStyle }>
         <h1>PoE Relic Checklist</h1>
         <List />
       </div>
