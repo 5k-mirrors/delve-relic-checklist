10a0947b2c686ae325c4702e68b5fcbd058155f9
Csaba Apagyi <csaba.apagyi@gmail.com>
2018-09-16 00:31:57 +0200

    Fixes path to github badge

--- src/GithubLinks/githubLinks.js
+++ src/GithubLinks/githubLinks.js
@@ -3,11 +3,13 @@ import React from 'react';
 import Link from './LinkTypes/link';
 import "../styles.css";
 
+import githubBadge from 'githubBadge';
+
 const githubLinks = props => (
   <div className="links">
     <Link linkTo="https://github.com/5k-mirrors/relic-checklist/issues/new">Report issue</Link>
     <Link linkTo="https://github.com/5k-mirrors/relic-checklist">
-      <img src="/public/github.svg" alt="GitHub" />
+      <img src={githubBadge} alt="GitHub" />
     </Link>
     <Link linkTo="https://github.com/5k-mirrors" text="5k-mirrors">5k-mirrors</Link>
   </div>
