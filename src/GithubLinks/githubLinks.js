bd9e9cbc41aea82db59571fe7ffbcefc38ba17bc
Gömöri Zsolt <gomori.zsolt1995@gmail.com>
2018-09-15 23:34:00 +0200

    SVG saved locally to the public folder

--- src/GithubLinks/githubLinks.js
+++ src/GithubLinks/githubLinks.js
@@ -7,7 +7,7 @@ const githubLinks = props => (
   <div className="links">
     <Link linkTo="https://github.com/5k-mirrors/relic-checklist/issues/new">Report issue</Link>
     <Link linkTo="https://github.com/5k-mirrors/relic-checklist">
-      <img src="https://img.shields.io/badge/-hosted%20with%20%E2%99%A5%EF%B8%8F%20by%20GitHub-%23c0c5ce.svg" alt="GitHub" />
+      <img src="/public/github.svg" alt="GitHub" />
     </Link>
     <Link linkTo="https://github.com/5k-mirrors" text="5k-mirrors">5k-mirrors</Link>
   </div>
