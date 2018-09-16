46b67a7883d5c87c86c62a4b124edbb6e166168b
Csaba Apagyi <csaba.apagyi@gmail.com>
2018-09-17 01:36:03 +0200

    Remove github badge temporarily to better fit style

--- src/GithubLinks/githubLinks.js
+++ src/GithubLinks/githubLinks.js
@@ -3,14 +3,10 @@ import React from 'react';
 import Link from './LinkTypes/link';
 import "../styles.css";
 
-import GithubBadge from 'GithubBadge';
-
 const githubLinks = props => (
   <div className="links">
     <Link linkTo="https://github.com/5k-mirrors/relic-checklist/issues/new">Report issue</Link>
-    <Link linkTo="https://github.com/5k-mirrors/relic-checklist">
-      <img src={GithubBadge} alt="GitHub" />
-    </Link>
+    <Link linkTo="https://github.com/5k-mirrors/relic-checklist">Hosted with ♥️ by GitHub</Link>
     <Link linkTo="https://github.com/5k-mirrors" text="5k-mirrors">5k-mirrors</Link>
   </div>
 );
