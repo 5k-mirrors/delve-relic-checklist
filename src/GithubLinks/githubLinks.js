7f289772bb257ad919beab410d8426ecb767f73e
Csaba Apagyi <csaba.apagyi@gmail.com>
2018-09-17 23:56:41 +0200

    Simplify and reorganize headline tests

--- src/GithubLinks/githubLinks.js
+++ src/GithubLinks/githubLinks.js
@@ -5,9 +5,9 @@ import "../styles.css";
 
 const githubLinks = props => (
   <div className="links">
+    <Link linkTo="https://github.com/5k-mirrors" text="5k-mirrors">Made by 5k-mirrors</Link>
+    <Link linkTo="https://github.com/5k-mirrors/relic-checklist">Hosted by GitHub</Link>
     <Link linkTo="https://github.com/5k-mirrors/relic-checklist/issues/new">Report issue</Link>
-    <Link linkTo="https://github.com/5k-mirrors/relic-checklist">Hosted with ♥️ by GitHub</Link>
-    <Link linkTo="https://github.com/5k-mirrors" text="5k-mirrors">5k-mirrors</Link>
   </div>
 );
 
