f5617b551110d035c5842c9b615347acdd80855d
Csaba Apagyi <csaba.apagyi@gmail.com>
2018-09-21 18:47:56 +0200

    Adds a special message to Chris

--- src/Footer/footer.js
+++ src/Footer/footer.js
@@ -9,7 +9,7 @@ const footer = props => (
       label="Legal"
       title="Legal"
       contentText="Path of Exile content and materials are trademarks and copyrights of Grinding Gear Games or its licensors.
-      This site is fan-made and not affiliated with Grinding Gear Games in any way."
+      This site is fan-made and not affiliated with Grinding Gear Games in any way. Unfortunately. C'mon Chris, give us a job offer."
     />
   </div>
 );
